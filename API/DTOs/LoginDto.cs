127db27fd98307cbf9e01df1dc09188a0495cbd8
Ryan Jeremy <ryan.jeremy.backend@gmail.com>
2025-03-21 16:47:32 +0100

    create LoginDto

--- API/DTOs/LoginDto.cs
+++ API/DTOs/LoginDto.cs
@@ -3,8 +3,8 @@ using System.ComponentModel.DataAnnotations;
 
 namespace API.DTOs;
 
-    public class LoginDto
-    {
+public class LoginDto
+{
         public required string Username { get; set; }
         public required string Password { get; set; }
-    }
+}
