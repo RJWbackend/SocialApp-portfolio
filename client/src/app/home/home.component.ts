1a518d9b766f8c24b123aec3d0091f58e195de8b
Ryan Jeremy <ryan.jeremy.backend@gmail.com>
2025-03-27 23:22:13 +0100

    fixes code format

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -9,23 +9,23 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit {
- http = inject(HttpClient);
- registerMode = false;
- users: any;
+  http = inject(HttpClient);
+  registerMode = false;
+  users: any;
 
- ngOnInit(): void {
+  ngOnInit(): void {
     this.getUsers();
- }
+  }
 
- registerToggle(){
-  this.registerMode =!this.registerMode;
- }
+  registerToggle() {
+    this.registerMode = !this.registerMode;
+  }
 
- getUsers() {
-  this.http.get('https:localhost:5001/api/users').subscribe({
-    next: response => this.users = response,
-    error: error => console.log(error),
-    complete: () => console.log('Request has completed')
-  })
-}
+  getUsers() {
+    this.http.get('https:localhost:5001/api/users').subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log('Request has completed')
+    })
+  }
 }
