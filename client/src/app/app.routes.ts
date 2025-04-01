dcd816bf17ec314bb912fe49fb054818758833b3
Ryan Jeremy <ryan.jeremy.backend@gmail.com>
2025-04-01 21:08:48 +0200

    created Middleware angular guards app routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,13 +4,22 @@ import { MemberDetailComponent } from './members/member-detail/member-detail.com
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
+import { authGuard } from './_guards/auth.guard';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'members', component: MemberListComponent},
-    {path: 'members/:id', component: MemberDetailComponent},
-    {path: 'lists', component: ListsComponent},
-    {path: 'messages', component: MessagesComponent},
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [authGuard],
+        children: [
+            {path: 'members', component: MemberListComponent,canActivate:[authGuard]},
+            {path: 'members/:id', component: MemberDetailComponent},
+            {path: 'lists', component: ListsComponent},
+            {path: 'messages', component: MessagesComponent},
+        ]
+    },
+    {path: 'errors', component: TestErrorsComponent},
     {path: '**', component: HomeComponent, pathMatch: 'full'},
-
 ];
