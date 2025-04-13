bfe4762cde59adf19516c55bfd53d3b710f5b722
Ryan Jeremy <ryan.jeremy.backend@gmail.com>
2025-04-13 18:53:46 +0200

    setMainPhoto in client create HttpDelete UsersController API

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/member';
+import { Photo } from '../_models/photo';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,16 +11,30 @@ import { Member } from '../_models/member';
 export class MembersService {
   private http = inject(HttpClient);
   baseUrl = environment.apiUrl;
-  
+  members = signal<Member[]>([]);
+
   getMembers() {
     return this.http.get<Member[]>(this.baseUrl + 'users');
   }
-  getMember(username:string) {
+  getMember(username: string) {
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
   updateMember(member: Member) {
     return this.http.put(this.baseUrl + 'users', member);
+  }
+
+  setMainPhoto(photo: Photo) {
+    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photo.id, {}).pipe(
+      tap(() => {
+        this.members.update(members => members.map(m => {
+          if (m.photos.includes(photo)) {
+            m.photoUrl = photo.url
+          }
+          return m;
+        }))
+      })
+    )
   }
 
 }
