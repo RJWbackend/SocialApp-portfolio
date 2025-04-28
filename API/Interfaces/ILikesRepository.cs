35692528e97198520c4328ef30eb9b2dd030d5a5
Ryan Jeremy <ryan.jeremy.backend@gmail.com>
2025-04-28 18:23:41 +0200

    pagination API side for likes

--- API/Interfaces/ILikesRepository.cs
+++ API/Interfaces/ILikesRepository.cs
@@ -1,12 +1,13 @@
 using System;
 using API.DTOs;
+using API.Helpers;
 
 namespace API.Interfaces;
 
 public interface ILikesRepository
 {
     Task<UserLike>GetUserLike(int sourceUserId, int targetUserId);
-    Task<IEnumerable<MemberDto>> GetUserLikes(string predicate, int userId);
+    Task<PagedList<MemberDto>> GetUserLikes(LikesParams likesParams);
     Task<IEnumerable<int>> GetCurrentUserLikeIds(int currentUserId);
     void DeleteLike(UserLike userLike);
 
